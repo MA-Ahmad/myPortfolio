f55f83a498114330db299798c19df2a55ac9a874
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-19 03:50:32 -0700

    Create types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+type project = {
+    id: number;
+    title: string;
+    logo: string;
+    link: string;
+    desc: string;
+    tech: string[];
+  };
+
+  type article = {
+    title: string;
+    desc: string;
+    link: string;
+    views: string;
+    published: string;
+    tags: string[];
+    github_url: string;
+  }
+  
