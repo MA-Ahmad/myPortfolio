09704a6d9bf7470d4274064eebe0f3273afde6ec
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-19 03:50:42 -0700

    created index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,17 @@
-import * as React from "react";
-import {
-  Flex,
-  Link,
-  IconButton,
-  useDisclosure,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { FaGithub } from "react-icons/fa";
-import { ColorModeSwitcher } from '../components/ColorModeSwitcher';
+import Home from '../components/home-page/home';
+import { projectsList } from "../data/projects-list";
 
-export default function Home() {
+export default function Index({ projects }) {
   return (
-    <Flex alignItems={"center"}>
-      <IconButton
-        as={Link}
-        href={"https://github.com/MA-Ahmad"}
-        size={"md"}
-        icon={<FaGithub />}
-        aria-label={"Github account"}
-        bg={useColorModeValue("white", "gray.700")}
-        _hover={{
-          textDecoration: "none",
-          bg: useColorModeValue("gray.200", "gray.900")
-        }}
-      />
-      <ColorModeSwitcher justifySelf="flex-end" />
-    </Flex>
+    <Home projects={projects} />
   )
+}
+
+export function getStaticProps() {
+  const projects = projectsList;
+  return {
+    props: {
+      projects
+    },
+  };
 }
