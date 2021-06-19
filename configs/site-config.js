d75e10cc2945a713d0e8845fa0085ded90587b26
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-19 03:51:17 -0700

    Create site-config.js

---
+++ configs/site-config.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  FaGithub,
+  FaDev,
+  FaLinkedin,
+  FaQuora,
+  FaTwitter
+} from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+
+const siteConfig = {
+  copyright: `Copyright © ${new Date().getFullYear()} Muhammad Ahmad. All Rights Reserved.`,
+  author: {
+    name: "Muhammad Ahmad",
+    accounts: [
+      {
+        url: "https://github.com/MA-Ahmad",
+        label: "Github Account",
+        type: "gray",
+        icon: <FaGithub />
+      },
+      {
+        url: "https://twitter.com/muhammad_ahmaad",
+        label: "Twitter Account",
+        type: "twitter",
+        icon: <FaTwitter />
+      },
+      {
+        url: "https://dev.to/m_ahmad",
+        label: "Dev Account",
+        type: "gray",
+        icon: <FaDev />
+      },
+      {
+        url: "https://linkedin.com/in/muhammad-ahmad20",
+        label: "LinkedIn Account",
+        type: "linkedin",
+        icon: <FaLinkedin />
+      },
+      {
+        url: "https://www.quora.com/profile/Muhammad-Ahmad-66",
+        label: "Quora Account",
+        type: "red",
+        icon: <FaQuora />
+      },
+      {
+        url: "mailto:muhammad.ahmad8043@gmail.com",
+        label: "Mail ahmad",
+        type: "gray",
+        icon: <FiMail />
+      }
+    ]
+  }
+};
+
+export default siteConfig;
