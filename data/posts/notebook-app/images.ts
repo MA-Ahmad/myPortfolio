5de73bec1736ad836efe6b5cc2863774c36eb153
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-19 05:25:24 -0700

    Delete images.ts

--- data/posts/notebook-app/images.ts
+++
@@ -1,8 +0,0 @@
-import img1 from "assets/images/posts/notebook-app/img_1.png";
-import img2 from "assets/images/posts/notebook-app/img_2.png";
-import img3 from "assets/images/posts/notebook-app/img_3.png";
-import img4 from "assets/images/posts/notebook-app/img_4.png";
-import img5 from "assets/images/posts/notebook-app/img_5.png";
-import img6 from "assets/images/posts/notebook-app/img_6.png";
-
-export const images = [img1, img2, img3, img4, img5, img6];
