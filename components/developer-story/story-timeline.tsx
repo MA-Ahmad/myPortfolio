1645eeb9b42edb90ec9c2db1fe891874ea77e465
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-06-20 08:47:54 -0700

    commented out width property

--- components/developer-story/story-timeline.tsx
+++ components/developer-story/story-timeline.tsx
@@ -45,7 +45,7 @@ export const StoryTimeline: React.FC<StoryTimelineProps> = ({
           placement={index % 2 === 0 ? "right" : "left"}
           closeOnBlur={false}
           // variant="responsive"
-          width={["9.3rem", "13rem", "15rem", "100%"]}
+          // width={["9.3rem", "13rem", "15rem", "100%"]}
         >
           <PopoverTrigger>
             <Box onClick={open} position="relative">
