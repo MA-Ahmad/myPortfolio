426c150bb5b6e4f1cbac9d8d7380226d3e73d2f9
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-07-23 08:00:07 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
-import App, { AppProps } from 'next/app'
-import { ChakraProvider } from "@chakra-ui/react"
-import Layout from '../components/layout/layout';
-import { theme } from '../components/ui/theme';
-
-import 'highlight.js/styles/railscasts.css'
+import App, { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "../components/layout/layout";
+import { theme } from "../components/ui/theme";
+import { PrismGlobal } from '../components/ui/prism'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS={true}>
+      <PrismGlobal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
-  )
+  );
 }
 
-MyApp.getInitialProps = async (appContext) => {
+MyApp.getInitialProps = async appContext => {
   const appProps = await App.getInitialProps(appContext);
   return { ...appProps };
 };
-export default MyApp
+export default MyApp;
