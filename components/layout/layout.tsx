cfd88c592ee10451fa9e5499904c485a69496e41
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-07-23 07:59:07 -0700

    Update layout.tsx

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,26 +1,26 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 import { Box } from "@chakra-ui/react";
 import TopNav from "./top-nav";
 import Footer from "./footer";
 
 function Layout(props) {
-    return (
-        <Fragment>
-            <TopNav />
-            <Box
-                textAlign="center"
-                fontSize="xl"
-                w={["90%", "85%", "80%"]}
-                maxW={800}
-                mx="auto"
-            >
-                <Box pt={10} pb={10}>
-                    {props.children}
-                </Box>
-            </Box>
-            <Footer />
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <TopNav />
+      <Box
+        textAlign="center"
+        fontSize="xl"
+        w={["90%", "85%", "80%"]}
+        maxW={800}
+        mx="auto"
+      >
+        <Box pt={10} pb={10}>
+          {props.children}
+        </Box>
+      </Box>
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default Layout;
