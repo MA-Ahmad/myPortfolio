997adf74008b923b0d3b8602936a4fd3b55ef97e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-07-23 11:58:41 -0700

    Update blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,25 +1,30 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 import { Stack } from "@chakra-ui/react";
 // import { articles } from "../../data/posts/articles";
 import PostCard from "../components/blog/card";
-import { PageSlideFade, StaggerChildren } from "../components/ui/page-transitions";
+import {
+  PageSlideFade,
+  StaggerChildren
+} from "../components/ui/page-transitions";
 import Header from "../components/layout/header";
 import { MotionBox } from "../components/ui/motion";
-import Meta from '../components/layout/meta';
-import { GetStaticProps } from 'next'
-import { getAllBlogArticles } from '../lib/devto'
-
+import Meta from "../components/layout/meta";
+import { GetStaticProps } from "next";
+import { getAllBlogArticles } from "../lib/devto";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
 const TURQUOISE = "#06b6d4";
 
 const Posts = ({ articles }) => {
   return (
     <Fragment>
-      <Meta title='Blog' description='A list of all articles and posts!' />
+      <Meta title="Blog" description="A list of all articles and posts!" />
       <PageSlideFade>
         <Header underlineColor={TURQUOISE} mt={0} mb={0}>
           Featured Articles
-      </Header>
+        </Header>
         <StaggerChildren>
           <Stack spacing={4} mt={12}>
             {articles.map((article, index) => (
@@ -34,10 +39,36 @@ const Posts = ({ articles }) => {
   );
 };
 
+const root = process.cwd();
+
 export const getStaticProps: GetStaticProps = async () => {
-  const articles = await getAllBlogArticles()
+  const paths = fs
+    .readdirSync(path.join(root, "data", "articles"))
+    .map(p => p.replace(/\.mdx/, ""));
+  console.log(paths);
+
+  let articlesLocal = [];
+  paths.map(p => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join(root, "data", "articles", `${p}.mdx`),
+      "utf-8"
+    );
+    const { data: frontmatter, content } = matter(markdownWithMeta);
+    articlesLocal.push({
+      slug: `blog/${p}`,
+      title: frontmatter.title,
+      description: frontmatter.description,
+      publishedAt: frontmatter.date,
+      tags: frontmatter.tags,
+      devToURL: null
+    });
+  });
+
+  const articlesDevto = await getAllBlogArticles();
+  const articles = [ ...articlesLocal, ...articlesDevto ]
+
   // Pass articles to the page via props
-  return { props: { articles } }
-}
+  return { props: { articles } };
+};
 
 export default Posts;
