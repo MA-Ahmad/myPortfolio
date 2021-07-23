d3e3a0ab0cc37c582c10f750c72649d6873003cf
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-07-23 11:53:01 -0700

    Update IArticle.ts

--- interfaces/IArticle.ts
+++ interfaces/IArticle.ts
@@ -1,22 +1,22 @@
 interface IArticle {
-    id: number
+    id?: number
     title: string
     description: string
-    coverImage: string
+    coverImage?: string
     tags: string[]
     publishedAt: string
-    devToSlug: string
-    devToPath: string
-    devToURL: string
-    commentsCount: number
-    publicReactionsCount: number
-    positiveReactionsCount: number
-    canonical: string
-    collectionId: number
+    devToSlug?: string
+    devToPath?: string
+    devToURL?: string
+    commentsCount?: number
+    publicReactionsCount?: number
+    positiveReactionsCount?: number
+    canonical?: string
+    collectionId?: number
     readingTime?: number
     slug: string
-    markdown: string
-    html: string
+    markdown?: string
+    html?: string
 }
 
 export default IArticle
