91f5ad4d5613fbe204d55c72022c7cfda520cbae
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-07-24 03:28:17 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,37 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <meta property="og:title" content="Muhammad Ahmad - Full Stack Developer" />
+          <meta
+            property="og:title"
+            content="Muhammad Ahmad - Full Stack Developer"
+          />
           <meta property="og:image" content="%PUBLIC_URL%/logo512.png" />
-          <link rel='icon' href='/favicon.ico' />
+          <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
             rel="stylesheet"
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
           />
         </Head>
         <body>
