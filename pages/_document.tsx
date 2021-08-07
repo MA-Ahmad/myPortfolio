ed940bf2dde25b18718b59384328e70d42de9b8f
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-08-07 06:47:26 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta
-            property="og:title"
-            content="Muhammad Ahmad - Full Stack Developer"
-          />
-          <meta property="og:image" content="%PUBLIC_URL%/logo512.png" />
           <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
