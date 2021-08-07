6454db8a5012fb61b1da17d69519e8857a6ca6d1
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-08-07 06:47:19 -0700

    Update meta.tsx

--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -1,22 +1,23 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-const Meta = ({ title, keywords, description }) => {
-    return (
-        <Head>
-            <meta name='viewport' content='width=device-width, initial-scale=1' />
-            <meta charSet='utf-8' />
-            <meta name='keywords' content={keywords} />
-            <meta name='description' content={description} />
-            {/* <link rel='icon' href='/favicon.ico' /> */}
-            <title>{title}</title>
-        </Head>
-    )
-}
+const Meta = ({ title, keywords, description, image }) => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta name="keywords" content={keywords} />
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={image ? image : "/logo512.png"} />
+      <title>{title}</title>
+    </Head>
+  );
+};
 
 Meta.defaultProps = {
-    title: 'Muhammad Ahmad - Full Stack Developer',
-    keywords: 'web development, programming, web design',
-    description: 'Software Engineer. Lover of web and opensource.',
-}
+  title: "Muhammad Ahmad - Full Stack Developer",
+  keywords: "web development, programming, web design",
+  description: "Software Engineer. Lover of web and opensource."
+};
 
-export default Meta
+export default Meta;
