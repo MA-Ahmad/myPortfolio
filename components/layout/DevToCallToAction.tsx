039e7da63c750338d487ea44f2eb3b051bda8d88
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-08-08 10:22:43 -0700

    Update DevToCallToAction.tsx

--- components/layout/DevToCallToAction.tsx
+++ components/layout/DevToCallToAction.tsx
@@ -1,4 +1,4 @@
-import { Flex, Link, Text, Icon } from "@chakra-ui/react";
+import { Flex, Link, Text, Icon, Box } from "@chakra-ui/react";
 import { FaDev } from "react-icons/fa";
 
 interface IProps {
@@ -7,12 +7,14 @@ interface IProps {
 
 const DevToCallToAction = ({ href }: IProps): JSX.Element => (
   <Flex justifyContent="center" marginTop="7">
-    <Link href={href} isExternal>
+    <Box>
       <Text marginBottom="2" fontSize="2xl" fontWeight="400">
         React, comment and follow on
       </Text>
-      <Icon as={FaDev} w={12} h={12} />
-    </Link>
+      <Link href={href} isExternal>
+        <Icon as={FaDev} w={12} h={12} />
+      </Link>
+    </Box>
   </Flex>
 );
 
