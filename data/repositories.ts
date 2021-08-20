43bbfcd72d1275dfb6e58ad5d2188fabe0595253
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-08-20 06:05:08 -0700

    Update repositories.ts

--- data/repositories.ts
+++ data/repositories.ts
@@ -30,7 +30,7 @@ export const repositories = [
     url: "https://github.com/MA-Ahmad/myPortfolio",
     live: "https://my-portfolio-ma-ahmad.vercel.app",
     blurHash: "L25#he^nryxc^-w$V{V_56bqx[M{",
-    stars: "25",
+    stars: "27",
     fork: "5"
   },
   {
