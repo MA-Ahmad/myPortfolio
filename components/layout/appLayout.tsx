6837d4d44e982168cc080835e2b381ac635f2273
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-01-16 16:35:03 +0500

    added dotted rectangles

--- components/layout/appLayout.tsx
+++ components/layout/appLayout.tsx
@@ -2,11 +2,13 @@ import { Fragment } from "react";
 import { Box } from "@chakra-ui/react";
 import TopNav from "./top-nav";
 import Footer from "./footer";
+import styles from './appLayout.module.css';
 
 function AppLayout(props) {
   return (
     <Fragment>
       <TopNav />
+      <DottedSvgs />
       <Box
         textAlign="center"
         fontSize="xl"
@@ -21,6 +23,35 @@ function AppLayout(props) {
       <Footer />
     </Fragment>
   );
+}
+
+function DottedSvgs() {
+  return (
+    <Box position="absolute" height="50rem" width="100%" overflow="hidden" display={['none', 'none', 'block']}>
+      <Box position="relative" height="full" mx="auto" maxW="960px">
+        <Box position="absolute" left="100%">
+          <svg className={`${styles.dottedRightTransform} ${styles.dottedColor}`} width="404" height="784" fill="none" viewBox="0 0 404 784">
+            <defs>
+              <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                <rect x="0" y="0" width="4" height="4" className="text-gray-700" fill="currentColor"></rect>
+              </pattern>
+            </defs>
+            <rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"></rect>
+          </svg>
+        </Box>
+        <Box position="absolute" right="100%">
+          <svg className={`${styles.dottedLeftTransform} ${styles.dottedColor}`} width="404" height="784" fill="none" viewBox="0 0 404 784">
+            <defs>
+              <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                <rect x="0" y="0" width="4" height="4" className="text-gray-700" fill="currentColor"></rect>
+              </pattern>
+            </defs>
+            <rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"></rect>
+          </svg>
+        </Box>
+      </Box>
+    </Box>
+  )
 }
 
 export default AppLayout;
