b79e127dda97090c5decbad4903d4f1827e15492
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-01-19 23:48:44 +0500

    removed @ts-ignore

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,11 +6,7 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  // Ensure the prisma instance is re-used during hot-reloading
-  // Otherwise, a new client will be created on every reload
-  // @ts-ignore
   global["prisma"] = global["prisma"] || new PrismaClient()
-  // @ts-ignore
   prisma = global["prisma"]
 }
 
