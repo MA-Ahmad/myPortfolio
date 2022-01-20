0cf0574b72b4a10e7a579c400fbce79d3de809f6
Josiah <96033624+drago26@users.noreply.github.com>
2022-01-19 22:06:24 -0500

    Update header.tsx

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -8,7 +8,7 @@ interface Props extends BoxProps {
 }
 
 const Header = ({ children, underlineColor, emoji, ...props }: PropsWithChildren<Props>) => (
-  <Box as="h1" mt={10} mb={6} fontSize="3xl" lineHeight="shorter" fontWeight="bold" {...props} textAlign="left">
+  <Box as="h1" mt={10} mb={6} fontSize="3xl" minW={"full"}  lineHeight="shorter" fontWeight="bold" {...props} textAlign="left">
     <UnderlinedText color={underlineColor}>{children}</UnderlinedText>
     {emoji ? ' ' + emoji : ''}
   </Box>
