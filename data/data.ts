ab494cbeac07b8a516d8ac0cd3e4e641f47d5689
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-01-30 23:34:35 +0500

    Updated new year content

--- data/data.ts
+++ data/data.ts
@@ -1,19 +1,19 @@
 export const companies = [
   {
-    title: "Phaedra Solutions",
-    alt: "PS image",
-    role: "Senior Software Engineer",
-    skills: ["ruby", "rails", "typescript", "javascript", "react", "aws"],
-    period: "2019 - Present",
-    logo: '/assets/images/logos/phaedra.jpeg'
+    title: 'Phaedra Solutions',
+    alt: 'PS image',
+    role: 'Senior Software Engineer',
+    skills: ['ruby', 'rails', 'typescript', 'javascript', 'react', 'aws'],
+    period: '2019 - Present',
+    logo: '/assets/images/logos/phaedra.jpeg',
   },
   {
-    title: "Brainx Technologies",
-    alt: "BT image",
-    role: "Software Engineer",
-    skills: ["ruby", "rails", "javascript", "python"],
-    period: "2018 - 2019",
-    logo: '/assets/images/logos/brainxtech.png'
+    title: 'Brainx Technologies',
+    alt: 'BT image',
+    role: 'Software Engineer',
+    skills: ['ruby', 'rails', 'javascript', 'python'],
+    period: '2018 - 2019',
+    logo: '/assets/images/logos/brainxtech.png',
   },
   // {
   //   title: "Codenya Studio",
@@ -24,176 +24,174 @@ export const companies = [
   //   logo: '/assets/images/logos/codenya.png'
   // },
   {
-    title: "Brainplow",
-    alt: "Brainplow image",
-    role: "Software Engineer",
-    skills: ["javascript", "python", "angular"],
-    period: "2017 - 2018",
-    logo: '/assets/images/logos/brainplow.jpg'
-  }
-];
+    title: 'Brainplow',
+    alt: 'Brainplow image',
+    role: 'Software Engineer',
+    skills: ['javascript', 'python', 'angular'],
+    period: '2017 - 2018',
+    logo: '/assets/images/logos/brainplow.jpg',
+  },
+]
 
 export const institutes = [
   {
-    short_title: "FAST - NUCES",
-    title: "National University of Computer and Emerging Sciences",
-    alt: "Nuces image",
+    short_title: 'FAST - NUCES',
+    title: 'National University of Computer and Emerging Sciences',
+    alt: 'Nuces image',
     role: "Bachelor's Degree in Computer Science",
-    skills: ["project management", "web development", "data structures"],
-    period: "2013 - 2017",
-    startingYear: "2013",
+    skills: ['project management', 'web development', 'data structures'],
+    period: '2013 - 2017',
+    startingYear: '2013',
     logo: '/assets/images/logos/fast.png',
     awards: [
       {
-        title: "Speed Programming Battle",
-        description:
-          "Won speed programming battle which held at FAST - NUCES.",
-        date: "May 1, 2015"
+        title: 'Speed Programming Battle',
+        description: 'Won speed programming battle which held at FAST - NUCES.',
+        date: 'May 1, 2015',
       },
       {
-        title: "Silver Medal",
+        title: 'Silver Medal',
         description:
-          "This medal was awarded to me by FAST - NUCES for getting a CGPA of 3.9 in 2nd semester.",
-        date: "May 1, 2014"
+          'This medal was awarded to me by FAST - NUCES for getting a CGPA of 3.9 in 2nd semester.',
+        date: 'May 1, 2014',
       },
       {
-        title: "Speed Programming Battle",
-        description:
-          "Won speed programming battle which held at FAST - NUCES.",
-        date: "Nov 1, 2013"
-      }
-    ]
-  }
-];
+        title: 'Speed Programming Battle',
+        description: 'Won speed programming battle which held at FAST - NUCES.',
+        date: 'Nov 1, 2013',
+      },
+    ],
+  },
+]
 
 export const skillsArray = [
   {
-    name: "Ruby on Rails",
-    description: "Web development",
-    link: "https://rubyonrails.org/",
-    type: "development",
-    image: '/assets/images/skills/rails.png'
+    name: 'Ruby on Rails',
+    description: 'Web development',
+    link: 'https://rubyonrails.org/',
+    type: 'development',
+    image: '/assets/images/skills/rails.png',
   },
   {
-    name: "React",
-    description: "Web development",
-    link: "https://reactjs.org/",
-    type: "development",
-    image: '/assets/images/skills/react.png'
+    name: 'React',
+    description: 'Web development',
+    link: 'https://reactjs.org/',
+    type: 'development',
+    image: '/assets/images/skills/react.png',
   },
   {
-    name: "Ruby",
-    description: "Web development, scrapping",
-    link: "https://www.ruby-lang.org/en/",
-    type: "development",
-    image: '/assets/images/skills/ruby.png'
+    name: 'Ruby',
+    description: 'Web development, scrapping',
+    link: 'https://www.ruby-lang.org/en/',
+    type: 'development',
+    image: '/assets/images/skills/ruby.png',
   },
   {
-    name: "Python",
-    description: "Web development, scrapping",
-    link: "https://www.python.org/",
-    type: "development",
-    image: '/assets/images/skills/python.png'
+    name: 'Python',
+    description: 'Web development, scrapping',
+    link: 'https://www.python.org/',
+    type: 'development',
+    image: '/assets/images/skills/python.png',
   },
   {
-    name: "Javascript",
-    description: "Web development",
-    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    type: "development",
-    image: '/assets/images/skills/javascript.png'
+    name: 'Javascript',
+    description: 'Web development',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    type: 'development',
+    image: '/assets/images/skills/javascript.png',
   },
   {
-    name: "Typescript",
-    description: "Web development",
-    link: "https://www.typescriptlang.org/",
-    type: "development",
-    image: '/assets/images/skills/typescript.png'
+    name: 'Typescript',
+    description: 'Web development',
+    link: 'https://www.typescriptlang.org/',
+    type: 'development',
+    image: '/assets/images/skills/typescript.png',
   },
   {
-    name: "Html5",
-    description: "Web layouts",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5",
-    type: "development",
-    image: '/assets/images/skills/html-5.png'
+    name: 'Html5',
+    description: 'Web layouts',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
+    type: 'development',
+    image: '/assets/images/skills/html-5.png',
   },
   {
-    name: "Css3",
-    description: "Web design",
-    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-    type: "design",
-    image: '/assets/images/skills/css3.png'
+    name: 'Css3',
+    description: 'Web design',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+    type: 'design',
+    image: '/assets/images/skills/css3.png',
   },
   {
-    name: "Bootstrap",
-    description: "Web design",
-    link: "https://getbootstrap.com/",
-    type: "design",
-    image: '/assets/images/skills/bootstrap.png'
+    name: 'Bootstrap',
+    description: 'Web design',
+    link: 'https://getbootstrap.com/',
+    type: 'design',
+    image: '/assets/images/skills/bootstrap.png',
   },
   {
-    name: "Postgres",
-    description: "Database",
-    link: "https://www.postgresql.org/",
-    type: "database",
-    image: '/assets/images/skills/postgresql.png'
+    name: 'Postgres',
+    description: 'Database',
+    link: 'https://www.postgresql.org/',
+    type: 'database',
+    image: '/assets/images/skills/postgresql.png',
   },
   {
-    name: "Mysql",
-    description: "Database",
-    link: "https://www.mysql.com/",
-    type: "database",
-    image: '/assets/images/skills/mysql.png'
+    name: 'Mysql',
+    description: 'Database',
+    link: 'https://www.mysql.com/',
+    type: 'database',
+    image: '/assets/images/skills/mysql.png',
   },
   {
-    name: "Redis",
-    description: "Database",
-    link: "https://redis.io/",
-    type: "database",
-    image: '/assets/images/skills/redis.png'
+    name: 'Redis',
+    description: 'Database',
+    link: 'https://redis.io/',
+    type: 'database',
+    image: '/assets/images/skills/redis.png',
   },
   {
-    name: "Heroku",
-    description: "Devops",
-    link: "https://www.heroku.com/",
-    type: "devops",
-    image: '/assets/images/skills/heroku.png'
+    name: 'Heroku',
+    description: 'Devops',
+    link: 'https://www.heroku.com/',
+    type: 'devops',
+    image: '/assets/images/skills/heroku.png',
   },
   {
-    name: "AWS",
-    description: "Devops",
-    link: "https://aws.amazon.com/",
-    type: "devops",
-    image: 'assets/images/skills/aws.png'
+    name: 'AWS',
+    description: 'Devops',
+    link: 'https://aws.amazon.com/',
+    type: 'devops',
+    image: 'assets/images/skills/aws.png',
   },
   {
-    name: "Digital Ocean",
-    description: "Devops",
-    link: "https://www.digitalocean.com/",
-    type: "devops",
-    image: '/assets/images/skills/digital-ocean.png'
+    name: 'Digital Ocean',
+    description: 'Devops',
+    link: 'https://www.digitalocean.com/',
+    type: 'devops',
+    image: '/assets/images/skills/digital-ocean.png',
   },
   {
-    name: "Nginx",
-    description: "Web server",
-    link: "https://www.nginx.com/",
-    type: "devops",
-    image: '/assets/images/skills/nginx.png'
+    name: 'Nginx',
+    description: 'Web server',
+    link: 'https://www.nginx.com/',
+    type: 'devops',
+    image: '/assets/images/skills/nginx.png',
   },
   {
-    name: "Visual Studio Code",
-    description: "Code editor of choice",
-    link: "https://code.visualstudio.com/",
-    type: "development",
-    image: '/assets/images/skills/vscode.png'
+    name: 'Visual Studio Code',
+    description: 'Code editor of choice',
+    link: 'https://code.visualstudio.com/',
+    type: 'development',
+    image: '/assets/images/skills/vscode.png',
   },
   {
-    name: "Figma",
-    description: "Collaborative Design",
-    link: "https://www.figma.com/",
-    type: "design",
-    image: '/assets/images/skills/figma.png'
-  }
-];
+    name: 'Figma',
+    description: 'Collaborative Design',
+    link: 'https://www.figma.com/',
+    type: 'design',
+    image: '/assets/images/skills/figma.png',
+  },
+]
 
 export const newContent = [
   {
@@ -202,8 +200,8 @@ export const newContent = [
     showNewTag: true,
   },
   {
-    link: '/projects',
-    text: 'Projects page',
+    link: '/changelog',
+    text: 'Created changelog page to track meaningful changes',
     showNewTag: true,
-  }
+  },
 ]
