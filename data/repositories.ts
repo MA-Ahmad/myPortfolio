c1918acdee861d39b57edcaaac1979db1d820c78
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-02-19 01:10:30 +0500

    Update repositories.ts

--- data/repositories.ts
+++ data/repositories.ts
@@ -7,8 +7,8 @@ export const repositories = [
     url: 'https://github.com/MA-Ahmad/myPortfolio',
     live: 'https://mahmad.me',
     blurHash: 'L25#he^nryxc^-w$V{V_56bqx[M{',
-    stars: '96',
-    fork: '22'
+    stars: '99',
+    fork: '23'
   },
   {
     title: 'Portfolio2',
