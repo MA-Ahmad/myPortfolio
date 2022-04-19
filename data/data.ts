cfaa838fa0a272f003fc561c639b6bb862241ab7
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-04-19 22:21:18 +0500

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -5,7 +5,7 @@ export const companies = [
     role: 'Senior Software Engineer',
     skills: ['ruby', 'rails', 'typescript', 'javascript', 'react', 'aws'],
     period: '2019 - Present',
-    logo: '/assets/images/logos/phaedra.jpeg',
+    logo: '/assets/images/logos/phaedra.jpeg'
   },
   {
     title: 'Brainx Technologies',
@@ -13,7 +13,7 @@ export const companies = [
     role: 'Software Engineer',
     skills: ['ruby', 'rails', 'javascript', 'python'],
     period: '2018 - 2019',
-    logo: '/assets/images/logos/brainxtech.png',
+    logo: '/assets/images/logos/brainxtech.png'
   },
   // {
   //   title: "Codenya Studio",
@@ -29,9 +29,9 @@ export const companies = [
     role: 'Software Engineer',
     skills: ['javascript', 'python', 'angular'],
     period: '2017 - 2018',
-    logo: '/assets/images/logos/brainplow.jpg',
-  },
-]
+    logo: '/assets/images/logos/brainplow.jpg'
+  }
+];
 
 export const institutes = [
   {
@@ -47,22 +47,22 @@ export const institutes = [
       {
         title: 'Speed Programming Battle',
         description: 'Won speed programming battle which held at FAST - NUCES.',
-        date: 'May 1, 2015',
+        date: 'May 1, 2015'
       },
       {
         title: 'Silver Medal',
         description:
           'This medal was awarded to me by FAST - NUCES for getting a CGPA of 3.9 in 2nd semester.',
-        date: 'May 1, 2014',
+        date: 'May 1, 2014'
       },
       {
         title: 'Speed Programming Battle',
         description: 'Won speed programming battle which held at FAST - NUCES.',
-        date: 'Nov 1, 2013',
-      },
-    ],
-  },
-]
+        date: 'Nov 1, 2013'
+      }
+    ]
+  }
+];
 
 export const skillsArray = [
   {
@@ -70,138 +70,137 @@ export const skillsArray = [
     description: 'Web development',
     link: 'https://rubyonrails.org/',
     type: 'development',
-    image: '/assets/images/skills/rails.png',
+    image: '/assets/images/skills/rails.png'
   },
   {
     name: 'React',
     description: 'Web development',
     link: 'https://reactjs.org/',
     type: 'development',
-    image: '/assets/images/skills/react.png',
+    image: '/assets/images/skills/react.png'
   },
   {
     name: 'Ruby',
     description: 'Web development, scrapping',
     link: 'https://www.ruby-lang.org/en/',
     type: 'development',
-    image: '/assets/images/skills/ruby.png',
+    image: '/assets/images/skills/ruby.png'
   },
   {
     name: 'Python',
     description: 'Web development, scrapping',
     link: 'https://www.python.org/',
     type: 'development',
-    image: '/assets/images/skills/python.png',
+    image: '/assets/images/skills/python.png'
   },
   {
     name: 'Javascript',
     description: 'Web development',
     link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
     type: 'development',
-    image: '/assets/images/skills/javascript.png',
+    image: '/assets/images/skills/javascript.png'
   },
   {
     name: 'Typescript',
     description: 'Web development',
     link: 'https://www.typescriptlang.org/',
     type: 'development',
-    image: '/assets/images/skills/typescript.png',
+    image: '/assets/images/skills/typescript.png'
   },
   {
     name: 'Html5',
     description: 'Web layouts',
     link: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
     type: 'development',
-    image: '/assets/images/skills/html-5.png',
+    image: '/assets/images/skills/html-5.png'
   },
   {
     name: 'Css3',
     description: 'Web design',
     link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
     type: 'design',
-    image: '/assets/images/skills/css3.png',
+    image: '/assets/images/skills/css3.png'
   },
   {
     name: 'Bootstrap',
     description: 'Web design',
     link: 'https://getbootstrap.com/',
     type: 'design',
-    image: '/assets/images/skills/bootstrap.png',
+    image: '/assets/images/skills/bootstrap.png'
   },
   {
     name: 'Postgres',
     description: 'Database',
     link: 'https://www.postgresql.org/',
     type: 'database',
-    image: '/assets/images/skills/postgresql.png',
+    image: '/assets/images/skills/postgresql.png'
   },
   {
     name: 'Mysql',
     description: 'Database',
     link: 'https://www.mysql.com/',
     type: 'database',
-    image: '/assets/images/skills/mysql.png',
+    image: '/assets/images/skills/mysql.png'
   },
   {
     name: 'Redis',
     description: 'Database',
     link: 'https://redis.io/',
     type: 'database',
-    image: '/assets/images/skills/redis.png',
+    image: '/assets/images/skills/redis.png'
   },
   {
     name: 'Heroku',
     description: 'Devops',
     link: 'https://www.heroku.com/',
     type: 'devops',
-    image: '/assets/images/skills/heroku.png',
+    image: '/assets/images/skills/heroku.png'
   },
   {
     name: 'AWS',
     description: 'Devops',
     link: 'https://aws.amazon.com/',
     type: 'devops',
-    image: 'assets/images/skills/aws.png',
+    image: 'assets/images/skills/aws.png'
   },
   {
     name: 'Digital Ocean',
     description: 'Devops',
     link: 'https://www.digitalocean.com/',
     type: 'devops',
-    image: '/assets/images/skills/digital-ocean.png',
+    image: '/assets/images/skills/digital-ocean.png'
   },
   {
     name: 'Nginx',
     description: 'Web server',
     link: 'https://www.nginx.com/',
     type: 'devops',
-    image: '/assets/images/skills/nginx.png',
+    image: '/assets/images/skills/nginx.png'
   },
   {
     name: 'Visual Studio Code',
     description: 'Code editor of choice',
     link: 'https://code.visualstudio.com/',
     type: 'development',
-    image: '/assets/images/skills/vscode.png',
+    image: '/assets/images/skills/vscode.png'
   },
   {
     name: 'Figma',
     description: 'Collaborative Design',
     link: 'https://www.figma.com/',
     type: 'design',
-    image: '/assets/images/skills/figma.png',
-  },
-]
+    image: '/assets/images/skills/figma.png'
+  }
+];
 
 export const newContent = [
   {
-    link: '/blog/started-2022-by-updating-portfolio-website-1jde-temp-slug-4553258',
-    text: 'Started 2022 by updating portfolio website',
-    showNewTag: true,
+    link: '/blog/templateskart-responsive-chakra-ui-projects-and-components-list',
+    text: 'TemplatesKart | Responsive Chakra UI Projects and Components list',
+    showNewTag: true
   },
   {
-    link: '/changelog',
-    text: 'Created changelog page to track meaningful changes',
-    showNewTag: true,
-  },
-]
+    link: '/blog/started-2022-by-updating-portfolio-website-1jde-temp-slug-4553258',
+    text: 'Started 2022 by updating portfolio website'
+  }
+];
