9e2696d49734dab98e21277622563f589303a9e2
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-04-25 00:23:11 +0500

    Add default SEO image

--- components/shared/meta.tsx
+++ components/shared/meta.tsx
@@ -1,11 +1,12 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { SEO_IMAGE } from 'data/constants';
 
 type Props = {
-  title: string
-  description?: string
-  keywords?: string
-  image?: string
-}
+  title: string;
+  description?: string;
+  keywords?: string;
+  image?: string;
+};
 
 const Meta = ({ title, keywords, description, image }: Props): JSX.Element => {
   return (
@@ -15,24 +16,23 @@ const Meta = ({ title, keywords, description, image }: Props): JSX.Element => {
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
-      <meta property="og:image" content={image ? image : '/logo512.png'} />
+      <meta property="og:image" content={image} />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={title} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image ? image : '/logo512.png'} />
+      <meta name="twitter:image" content={image} />
       <meta name="twitter:card" content="summary_large_image" />
-      <title>
-        {title.includes('Ahmad') ? title : title.concat(' | Muhammad Ahmad')}
-      </title>
+      <title>{title.includes('Ahmad') ? title : title.concat(' | Muhammad Ahmad')}</title>
     </Head>
-  )
-}
+  );
+};
 
 Meta.defaultProps = {
   title: 'Muhammad Ahmad - Full Stack Developer',
   keywords: 'web development, programming, web design, react js, chakra ui',
   description: 'Software Engineer. Lover of web and opensource.',
-}
+  image: SEO_IMAGE
+};
 
-export default Meta
+export default Meta;
--- data/constants.ts
+++ data/constants.ts
@@ -1,5 +1,12 @@
-export const GITHUB_API_URL = 'https://api.github.com'
-export const GITHUB_USERNAME = 'MA-Ahmad'
-export const GITHUB_REPO = 'myportfolio'
+/**
+ * LINKS and USERNAMES
+ */
+export const GITHUB_API_URL = 'https://api.github.com';
+export const GITHUB_USERNAME = 'MA-Ahmad';
+export const GITHUB_REPO = 'myportfolio';
+export const DEVTO_API_URL = 'https://dev.to/api';
 
-export const DEVTO_API_URL = 'https://dev.to/api'
+/**
+ * SEO constants
+ */
+export const SEO_IMAGE = 'https://mahmad.me/logo.png';
