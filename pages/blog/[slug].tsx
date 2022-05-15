6f2b7958de4b9fd6b3c9a65f22dea52d00330111
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-05-15 17:40:46 +0500

    Fix cover image shadow issue

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
-import { useState, useEffect } from 'react'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import html from 'remark-html'
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import html from 'remark-html';
 import {
   Box,
   Heading,
@@ -16,59 +16,55 @@ import {
   Collapse,
   Image,
   AspectRatio,
-  Skeleton,
-} from '@chakra-ui/react'
-import remark from 'remark'
-import prism from 'remark-prism'
-import { getTagColor } from '../../components/theme'
-import dayjs from 'dayjs'
-import localizedFormat from 'dayjs/plugin/localizedFormat'
-import { BlogPost } from '../../interfaces/interface'
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import PageLayout from 'components/layouts/pageLayout'
-import { MotionBox } from 'components/shared/animations/motion'
-import DevToCallToAction from 'components/shared/DevToCallToAction'
-import {
-  fadeInUp,
-  stagger,
-} from 'components/shared/animations/page-transitions'
-import { motion } from 'framer-motion'
-import { usePostData } from 'lib/usePostData'
-import { LikeButton } from 'components/shared/LikeButton'
-import { useLinkColor } from 'components/theme'
-import { getDevtoPosts } from 'lib/fetchPosts'
-import { HeartIcon, CommentIcon, EyeIcon } from 'components/shared/icons'
-import DisplayText from 'components/shared/icons/DisplayText'
+  Skeleton
+} from '@chakra-ui/react';
+import remark from 'remark';
+import prism from 'remark-prism';
+import { getTagColor } from '../../components/theme';
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import { BlogPost } from '../../interfaces/interface';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import PageLayout from 'components/layouts/pageLayout';
+import { MotionBox } from 'components/shared/animations/motion';
+import DevToCallToAction from 'components/shared/DevToCallToAction';
+import { fadeInUp, stagger } from 'components/shared/animations/page-transitions';
+import { motion } from 'framer-motion';
+import { usePostData } from 'lib/usePostData';
+import { LikeButton } from 'components/shared/LikeButton';
+import { useLinkColor } from 'components/theme';
+import { getDevtoPosts } from 'lib/fetchPosts';
+import { HeartIcon, CommentIcon, EyeIcon } from 'components/shared/icons';
+import DisplayText from 'components/shared/icons/DisplayText';
 
-dayjs.extend(localizedFormat)
+dayjs.extend(localizedFormat);
 
 export interface AllBlogProps {
-  blogDetails: BlogPost
-  articleContent: string
+  blogDetails: BlogPost;
+  articleContent: string;
 }
 
-const POST_VIEW_LIMIT = 100
+const POST_VIEW_LIMIT = 100;
 
-const ArticlePage: NextPage<AllBlogProps> = ({
-  articleContent,
-  blogDetails,
-}) => {
-  const { totalPostLikes, totalPostViews, isLoading, incrementViews } =
-    usePostData(blogDetails?.slug, blogDetails?.title)
-  const [showLikeButton, setShowLikeButton] = useState(false)
-  const borderColor = useColorModeValue('transparent', 'gray.700')
-  const linkColor = useLinkColor()
+const ArticlePage: NextPage<AllBlogProps> = ({ articleContent, blogDetails }) => {
+  const { totalPostLikes, totalPostViews, isLoading, incrementViews } = usePostData(
+    blogDetails?.slug,
+    blogDetails?.title
+  );
+  const [showLikeButton, setShowLikeButton] = useState(false);
+  const borderColor = useColorModeValue('transparent', 'gray.700');
+  const linkColor = useLinkColor();
 
   useEffect(() => {
-    incrementViews()
-    window.addEventListener('scroll', listenToScroll)
-    return () => window.removeEventListener('scroll', listenToScroll)
-  }, [])
+    incrementViews();
+    window.addEventListener('scroll', listenToScroll);
+    return () => window.removeEventListener('scroll', listenToScroll);
+  }, []);
 
   const listenToScroll = () => {
-    if (window.scrollY > 150) setShowLikeButton(true)
-    else setShowLikeButton(false)
-  }
+    if (window.scrollY > 150) setShowLikeButton(true);
+    else setShowLikeButton(false);
+  };
 
   return (
     <PageLayout
@@ -78,12 +74,7 @@ const ArticlePage: NextPage<AllBlogProps> = ({
       keywords={blogDetails?.tags.join(', ')}
     >
       <Collapse in={showLikeButton} animateOpacity>
-        <Box
-          position="fixed"
-          right="10%"
-          top="50%"
-          display={['none', 'none', 'none', 'block']}
-        >
+        <Box position="fixed" right="10%" top="50%" display={['none', 'none', 'none', 'block']}>
           <LikeButton
             id={blogDetails?.slug}
             title={blogDetails?.title}
@@ -95,7 +86,7 @@ const ArticlePage: NextPage<AllBlogProps> = ({
       <motion.div initial="initial" animate="animate" variants={stagger}>
         <VStack marginBottom="5" alignItems="left" textAlign="left">
           {blogDetails?.cover_image && (
-            <MotionBox whileHover={{ scale: 1.02 }} shadow="lg">
+            <MotionBox whileHover={{ scale: 1.02 }}>
               <AspectRatio ratio={1.85 / 1} w="100%" h="100%" rounded="xl">
                 <Image
                   src={blogDetails?.cover_image}
@@ -131,12 +122,7 @@ const ArticlePage: NextPage<AllBlogProps> = ({
             >
               <HStack spacing={1} alignItems="center">
                 {blogDetails?.tags.map((tag) => (
-                  <Tag
-                    size={'md'}
-                    padding="0 3px"
-                    key={tag}
-                    colorScheme={getTagColor(tag)}
-                  >
+                  <Tag size={'md'} padding="0 3px" key={tag} colorScheme={getTagColor(tag)}>
                     {tag}
                   </Tag>
                 ))}
@@ -146,10 +132,7 @@ const ArticlePage: NextPage<AllBlogProps> = ({
                   <Flex alignItems="center">
                     <DisplayText
                       isLoading={isLoading}
-                      value={
-                        (Number(blogDetails.public_reactions_count) || 0) +
-                        totalPostLikes
-                      }
+                      value={(Number(blogDetails.public_reactions_count) || 0) + totalPostLikes}
                     />
                     &nbsp;
                     <HeartIcon />
@@ -159,10 +142,7 @@ const ArticlePage: NextPage<AllBlogProps> = ({
                 )}
                 {blogDetails?.comments_count ? (
                   <Flex alignItems="center">
-                    <DisplayText
-                      isLoading={false}
-                      value={blogDetails.comments_count}
-                    />
+                    <DisplayText isLoading={false} value={blogDetails.comments_count} />
                     &nbsp;
                     <CommentIcon />
                   </Flex>
@@ -182,11 +162,7 @@ const ArticlePage: NextPage<AllBlogProps> = ({
             </HStack>
           </motion.div>
           <motion.div variants={fadeInUp}>
-            <HStack
-              spacing={2}
-              alignItems="left"
-              justifyContent={['center', 'left', 'left']}
-            >
+            <HStack spacing={2} alignItems="left" justifyContent={['center', 'left', 'left']}>
               <Text fontSize="xs">Published on</Text>
               <Text fontSize="xs" fontWeight="bold">
                 {dayjs(blogDetails?.published_at).format('LL')}
@@ -202,90 +178,85 @@ const ArticlePage: NextPage<AllBlogProps> = ({
         {blogDetails?.url ? <DevToCallToAction href={blogDetails.url} /> : ''}
       </motion.div>
     </PageLayout>
-  )
-}
+  );
+};
 
-const root = process.cwd()
+const root = process.cwd();
 export const getStaticPaths: GetStaticPaths = async () => {
-  let devData: BlogPost[] = await getDevtoPosts()
-  devData = devData.filter((data) => data.canonical_url.includes('dev.to'))
+  let devData: BlogPost[] = await getDevtoPosts();
+  devData = devData.filter((data) => data.canonical_url.includes('dev.to'));
   const devtoPaths = devData.map((data) => ({
-    params: { slug: data?.slug },
-  }))
+    params: { slug: data?.slug }
+  }));
 
-  const localPaths = fs
-    .readdirSync(path.join(root, 'data', 'posts'))
-    .map((p) => ({
-      params: {
-        slug: p.replace(/\.mdx/, ''),
-      },
-    }))
+  const localPaths = fs.readdirSync(path.join(root, 'data', 'posts')).map((p) => ({
+    params: {
+      slug: p.replace(/\.mdx/, '')
+    }
+  }));
 
   return {
     paths: [...devtoPaths, ...localPaths],
-    fallback: true,
-  }
-}
+    fallback: true
+  };
+};
 
 const markdownToHtml = async (markdown: string) => {
-  const result = await remark().use(html).use(prism).process(markdown)
-  return result.toString()
-}
+  const result = await remark().use(html).use(prism).process(markdown);
+  return result.toString();
+};
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const devData: BlogPost[] = await getDevtoPosts()
+  const devData: BlogPost[] = await getDevtoPosts();
 
   const selectedBlog = devData.filter(
-    (data) =>
-      data?.slug === params?.slug && data.canonical_url.includes('dev.to')
-  )
+    (data) => data?.slug === params?.slug && data.canonical_url.includes('dev.to')
+  );
   let blogObj = null,
-    remarkContent = null
+    remarkContent = null;
 
   if (selectedBlog.length) {
-    const res = await fetch(
-      `https://dev.to/api/articles/${selectedBlog[0]?.id}`
-    )
-    blogObj = await res.json()
-    remarkContent = await markdownToHtml(blogObj.body_markdown)
+    const res = await fetch(`https://dev.to/api/articles/${selectedBlog[0]?.id}`);
+    blogObj = await res.json();
+    remarkContent = await markdownToHtml(blogObj.body_markdown);
   } else {
     const markdownWithMeta = fs.readFileSync(
       path.join(root, 'data', 'posts', `${params?.slug}.mdx`),
       'utf-8'
-    )
-    const { data: frontmatter, content } = matter(markdownWithMeta)
+    );
+    const { data: frontmatter, content } = matter(markdownWithMeta);
     const devtoPost = devData.filter(
       (data) =>
         !data.canonical_url.includes('dev.to') &&
         data.canonical_url.split('/blog/')[1] === params?.slug
-    )[0]
+    )[0];
     if (devtoPost) {
-      frontmatter['comments_count'] = devtoPost?.comments_count
-      frontmatter['public_reactions_count'] = devtoPost?.public_reactions_count
-      frontmatter['url'] = devtoPost?.url
+      frontmatter['comments_count'] = devtoPost?.comments_count;
+      frontmatter['public_reactions_count'] = devtoPost?.public_reactions_count;
+      frontmatter['url'] = devtoPost?.url;
     }
-    blogObj = frontmatter
+    blogObj = frontmatter;
 
     // If slug not existed in blogObj
     if (params?.slug) {
-      blogObj.slug = params?.slug
+      blogObj.slug = params?.slug;
     }
-    remarkContent = await markdownToHtml(content)
+    remarkContent = await markdownToHtml(content);
   }
 
   if (!devData) {
     return {
-      notFound: true,
-    }
+      notFound: true
+    };
   }
 
   return {
     props: {
       articleContent: remarkContent,
-      blogDetails: blogObj,
+      blogDetails: blogObj
     },
-    revalidate: 1,
-  }
-}
+    revalidate: 1
+  };
+};
 
-export default ArticlePage
+export default ArticlePage;
