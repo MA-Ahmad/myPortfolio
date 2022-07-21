6df2d71f0d80d311c2f60fa64bc60252d3e9de19
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-07-21 21:03:17 +0500

    Update repositories.ts

--- data/repositories.ts
+++ data/repositories.ts
@@ -8,7 +8,7 @@ export const repositories = [
     live: 'https://mahmad.me',
     blurHash: 'L25#he^nryxc^-w$V{V_56bqx[M{',
     stars: '131',
-    fork: '28'
+    fork: '29'
   },
   {
     title: 'Portfolio2',
@@ -41,7 +41,7 @@ export const repositories = [
     url: 'https://github.com/MA-Ahmad/templatesKart',
     live: 'https://templateskart.com',
     blurHash: 'LMMaw^IV~pxu00%LRjNGAIIVadt6',
-    stars: '28',
+    stars: '29',
     fork: '5'
   },
   {
