4c844bb7c7669cd9e706d2c3e3fad518fa299d61
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2022-09-25 23:37:47 +0500

    Update repositories.ts

--- data/repositories.ts
+++ data/repositories.ts
@@ -41,7 +41,7 @@ export const repositories = [
     url: 'https://github.com/MA-Ahmad/templatesKart',
     live: 'https://templateskart.com',
     blurHash: 'LMMaw^IV~pxu00%LRjNGAIIVadt6',
-    stars: '43',
+    stars: '44',
     fork: '8'
   },
   {
