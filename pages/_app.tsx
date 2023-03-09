3e1c10040d5df7ecac4c9ecf9794fc95c99da333
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2023-03-10 00:47:05 +0500

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,34 @@
-import { useEffect } from 'react'
-import App, { AppProps } from 'next/app'
-import { ChakraProvider, Box } from '@chakra-ui/react'
-import AppLayout from 'components/layouts/appLayout'
-import { PrismGlobal } from 'components/theme/prism'
-import { useRouter } from 'next/router'
-import * as gtag from 'lib/gtag'
-import { AnimatePresence } from 'framer-motion'
-import { theme } from 'components/theme'
-import { AccentGlobal } from 'components/theme/Accent'
-import { FontsGlobal } from 'components/theme/fonts'
+import { useEffect } from 'react';
+import App, { AppProps } from 'next/app';
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import AppLayout from 'components/layouts/appLayout';
+import { PrismGlobal } from 'components/theme/prism';
+import { useRouter } from 'next/router';
+import * as gtag from 'lib/gtag';
+import { AnimatePresence } from 'framer-motion';
+import { theme } from 'components/theme';
+import { AccentGlobal } from 'components/theme/Accent';
+import { FontsGlobal } from 'components/theme/fonts';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <ChakraProvider theme={theme} resetCSS={true}>
       <FontsGlobal />
       <AccentGlobal />
       <PrismGlobal />
+      <Analytics />
       <AppLayout>
         <AnimatePresence
           exitBeforeEnter
@@ -39,11 +41,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         </AnimatePresence>
       </AppLayout>
     </ChakraProvider>
-  )
+  );
 }
 
 MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext)
-  return { ...appProps }
-}
-export default MyApp
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps };
+};
+export default MyApp;
